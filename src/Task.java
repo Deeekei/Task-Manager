b365c6e7062efdcabffac30f94007c27baadb6aa
Deee_kei <dmitrij.katkov2000@gmail.com>
2025-07-13 03:33:14 +0300

    added some methods to task manager"

--- src/Task.java
+++ src/Task.java
@@ -1,15 +1,11 @@
 public class Task {
     protected String taskName;
     protected String taskDescription;
-    protected int taskId;
-    protected static int idCounter;
     protected TaskStatuses taskStatus;
 
     protected Task(String taskName, String taskDescription, TaskStatuses taskStatus) {
         this.taskName = taskName;
         this.taskDescription = taskDescription;
         this.taskStatus = taskStatus;
-        taskId = idCounter;
-        idCounter++;
     }
 }
--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,4 +1,40 @@
 import java.util.HashMap;
+import java.util.ArrayList;
+
 public class TaskManager {
+    Integer taskIdCounter;
+    HashMap<Integer, Task> taskMap;
+    HashMap<Integer, Epic> epicMap;
+
+    public void createNewTask(Task task) {
+        Integer taskId = ++taskIdCounter;
+        taskMap.put(taskId, task);
+    }
+
+    public void showAllTasks() {
+        for(Task task : taskMap.values()){
+            System.out.println(task);
+        }
+    }
+    public void findTaskById(Integer taskId){
+        if (taskMap.containsKey(taskId)){
+            System.out.println(taskMap.get(taskId));
+        }
+        else {
+            System.out.println("Нет такого ID");
+        }
+
+    }
+    public void removeAllTasks(){
+        for (Integer taskId : taskMap.keySet()){
+            taskMap.remove(taskId);
+        }
+    }
+    public void removeTaskById(Integer taskId){
+        taskMap.remove(taskId);
+    }
+    public void replaceTaskById(Integer taskId, Task newTask){
+        taskMap.replace(taskId, newTask);
+    }
 
 }
